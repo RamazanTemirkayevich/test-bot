00a5f164fcb22c3167e92a056ef28852baef317d
Ramazan Bakshysh <family@Air-Family.lan>
2023-03-27 22:33:44 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const TelegramApi = require('node-telegram-bot-api')
+
+const token = '6062620121:AAGDAcYYnlcOzP6Q4EL9QqdffZrQJyrNxIY'
+
+const bot = new TelegramApi(token, {polling: true})
+
+const Options = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: 'Предложить новость'}]
+        ]
+    })
+}
+
+const start = () => {
+
+    bot.setMyCommands([
+        {command: '/start', description: 'Приветствие'},
+        {command: '/news', description: 'Предоставить информацию'},
+    ])
+
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+
+        if (text === '/start') {
+            return bot.sendMessage(chatId, `Добро пожаловать в телеграм бот "Крымские решения"`);
+        }
+        if (text === '/news') {
+            return bot.sendMessage(chatId, 'Хотите предложить новость?')
+        }
+
+        if (text === 'Предложить новость') {
+            return bot.sendMessage(chatId, 'Опишите ваше предложение')
+        }
+
+        return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!)');
+    })
+}
+
+start()
